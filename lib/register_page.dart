fbb54b574ef8c77fbe1e035b06111dc5a5e2a79e
HiroshiM10 <91099235+HiroshiM10@users.noreply.github.com>
2024-05-19 14:38:30 -0300

    Update register_page.dart

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,26 +1,154 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:trabalho_casseb/hamburgueria_list.dart';
+
+// import 'login_page.dart';
+
+// class RegisterPage extends StatelessWidget {
+//   final TextEditingController emailController = TextEditingController();
+//   final TextEditingController passwordController = TextEditingController();
+
+//   void _registerUser(BuildContext context) async {
+//     try {
+//       // ignore: unused_local_variable
+//       UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+//         email: emailController.text,
+//         password: passwordController.text,
+//       );
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(builder: (context) => HamburgueriaList()),
+//       );
+//     } catch (e) {
+//         print('Error: $e');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         automaticallyImplyLeading: false,
+//         title: const Text('Registrar'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             TextField(
+//               controller: emailController,
+//               decoration: const InputDecoration(
+//                 labelText: 'Email',
+//               ),
+//             ),
+//             TextField(
+//               controller: passwordController,
+//               decoration: const InputDecoration(
+//                 labelText: 'Senha',
+//               ),
+//               obscureText: true,
+//             ),
+//             const SizedBox(height: 16.0),
+//             ElevatedButton(
+//               onPressed: () => _registerUser(context),
+//               child: const Text('Registrar'),
+//             ),
+//             const SizedBox(height: 16.0),
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => LoginPage()),
+//                 );
+//               },
+//               child: const Text('Já tem uma conta? Faça login aqui'),
+//             ),
+//             // const SizedBox(height: 16.0),
+//             // TextButton(
+//             //   onPressed: () {
+//             //     Navigator.push(
+//             //       context,
+//             //       MaterialPageRoute(builder: (context) => ForgetPasswordPage()),
+//             //     );
+//             //   },
+//             //   child: const Text('Esqueceu a senha?'),
+//             // ),
+//             IconButton(
+//             onPressed: () async {
+             
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => HamburgueriaList()),
+//                 );
+              
+//             },
+//             icon: Icon(Icons.login),
+//           ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:trabalho_hamburguerias/hamburgueria_list.dart';
 
-import 'login_page.dart';
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
 
-class RegisterPage extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+class _RegisterPageState extends State<RegisterPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
 
-  void _registerUser(BuildContext context) async {
+  void _register() async {
+    if (_passwordController.text != _confirmPasswordController.text) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Erro'),
+          content: Text('As senhas não coincidem.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
     try {
-      // ignore: unused_local_variable
-      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: emailController.text,
-        password: passwordController.text,
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
       );
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => HamburgueriaList()),
-      );
+
+      if (userCredential.user != null) {
+        Navigator.pop(context);
+      }
     } catch (e) {
-        print('Error: $e');
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Erro'),
+          content: Text(e.toString()),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
     }
   }
 
@@ -28,62 +156,31 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Registrar'),
+        title: Text('Registrar'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
             TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
             ),
             TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Senha',
-              ),
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Senha'),
               obscureText: true,
             ),
-            const SizedBox(height: 16.0),
+            TextField(
+              controller: _confirmPasswordController,
+              decoration: InputDecoration(labelText: 'Confirmar Senha'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () => _registerUser(context),
-              child: const Text('Registrar'),
+              onPressed: _register,
+              child: Text('Registrar'),
             ),
-            const SizedBox(height: 16.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: const Text('Já tem uma conta? Faça login aqui'),
-            ),
-            // const SizedBox(height: 16.0),
-            // TextButton(
-            //   onPressed: () {
-            //     Navigator.push(
-            //       context,
-            //       MaterialPageRoute(builder: (context) => ForgetPasswordPage()),
-            //     );
-            //   },
-            //   child: const Text('Esqueceu a senha?'),
-            // ),
-            IconButton(
-            onPressed: () async {
-             
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HamburgueriaList()),
-                );
-              
-            },
-            icon: Icon(Icons.login),
-          ),
           ],
         ),
       ),
